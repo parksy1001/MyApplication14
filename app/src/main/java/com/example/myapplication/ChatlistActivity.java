62be913ed966091843c62b35bd984b251ba276a5
parksy1001 <57057806+parksy1001@users.noreply.github.com>
2020-05-19 21:00:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ChatlistActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class ChatlistActivity extends AppCompatActivity {
+
+
+    private RecyclerView recyclerView;
+    com.example.myapplication.MyAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatlist);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        String[] myDataset = {"test1", "test2", "test3"};
+        mAdapter = new com.example.myapplication.MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
+    }
+}
